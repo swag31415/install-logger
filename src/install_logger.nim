ccc4c56fe367bc82ae3fadc1b871d96ff1e1c026
swag31415 <kidswagatam18@gmail.com>
2021-01-27 22:37:51 -0500

    Made the logger only log to the first table

--- src/install_logger.nim
+++ src/install_logger.nim
@@ -8,12 +8,12 @@ const logfile = "Log3.md"
 var log = try: readFile(logfile) except: "|Timestamp|Software|\n|-|-|\n"
 
 echo "Enter program name to log it. Leave blank to do something else"
-let filename = readLine(stdin)
-if filename == "":
+let progname = readLine(stdin)
+if progname == "":
   quit(0)
 else:
-  let entry = "|" & now().format("M/dd/yyyy h:mmtt") & "|" & filename & "|\n"
-  logfile.writeFile(
-    if "\n\n" in log: log.replace("\n\n", "\n" & entry & "\n")
-    else: log & entry
-  )
+  let entry = "|" & now().format("M/dd/yyyy h:mmtt") & "|" & progname & "|\n"
+  if "\n\n" in log:
+    log.insert(entry, log.find("\n\n") + 1)
+  else: log.add(entry)
+  logfile.writeFile(log)
